63c832a2d68162a8b30e6173d12fd28e67a14d48
Kfeliciano94 <Krystalifeliciano@gmail.com>
2024-04-04 16:14:31 +0200

    Update
    Added to HTML, CSS, and JS

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ function refreshWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
+  let dateElement = document.querySelector("#date");
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
+
+  timeElement.textContent = date.toLocaleTimeString();
+  dateElement.textContent = date.toLocaleDateString();
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
@@ -16,6 +20,26 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
